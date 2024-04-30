8effbf5f17970cd8e04d8df2ff0aa5d78a3d878d
Svitlana Bublyk <lana.bublyk@gmail.com>
2024-04-30 13:40:35 +0200

    add function to call yt-dlp

--- Sources/SoundTransfer.swift
+++ Sources/SoundTransfer.swift
@@ -5,10 +5,42 @@
 // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
 
 import ArgumentParser
+import Foundation
 
 @main
 struct SoundTransfer: ParsableCommand {
     mutating func run() throws {
-        print("Hello, world!")
+
     }
 }
+
+func downloadFile( nameFile:String, urlYouTube:String) {
+    let task = Process()
+    let executableUrl = URL(fileURLWithPath: "/usr/local/bin/yt-dlp") 
+    task.executableURL = executableUrl
+    let pipeOut = Pipe()
+    task.standardOutput = pipeOut
+    let pipeError = Pipe()
+    task.standardError = pipeError
+    let options = ["--output", nameFile, "--extract-audio", "--audio-format", "mp3", urlYouTube]
+    task.arguments = options
+
+    do {
+        try task.run()
+        task.waitUntilExit()
+    } catch {
+        print("Unexpected error: \(error)")
+    }
+
+    let out = pipeOut.fileHandleForReading.readDataToEndOfFile()
+    let err = pipeError.fileHandleForReading.readDataToEndOfFile()
+
+    let outString = String(data: out, encoding: String.Encoding.utf8) ?? ""
+    let errString = String(data: err, encoding: String.Encoding.utf8) ?? ""
+
+    print(outString)
+    print(errString)
+
+    print("File downloaded ")
+}
+
