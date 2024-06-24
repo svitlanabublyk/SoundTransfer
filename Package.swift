0defaeb65a65bc2516011b5e26b103f3a40d1b62
Svitlana Bublyk <lana.bublyk@gmail.com>
2024-06-24 13:31:23 +0200

    install DocC plugin

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "SoundTransfer",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
